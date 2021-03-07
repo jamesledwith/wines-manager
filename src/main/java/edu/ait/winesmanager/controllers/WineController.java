0a05dab8616ae8c84ca900396de9f9291fdb853a
jamesledwith <jamesledwith@live.ie>
2021-03-07 18:50:26 +0000

    Update Controller and DAO service

--- src/main/java/edu/ait/winesmanager/controllers/WineController.java
+++ src/main/java/edu/ait/winesmanager/controllers/WineController.java
@@ -12,6 +12,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("api/v1")
 public class WineController {
@@ -64,3 +65,58 @@ public class WineController {
         }
     }
 }
+
+/*
+@RestController
+@RequestMapping("api/v1")
+public class WineController {
+
+    @Autowired
+    WineDAO wineDAO;
+
+    @GetMapping("wines")
+    public List<Wine> getAllWines(){
+        return wineDAO.findAll();
+    }
+
+    @GetMapping("wines/{wineId}")
+    public Optional<Wine> getWineById(@PathVariable("wineId") int param){
+        return wineDAO.findById(param);
+    }
+
+    @DeleteMapping("wines/{wineId}")
+    public void deleteWineById(@PathVariable int wineId){
+        wineDAO.deleteWine(wineId);
+    }
+
+    @PostMapping("wines/")
+    public ResponseEntity createWine(@RequestBody Wine newWine){
+        //add the wine
+        wineDAO.createWine(newWine);
+        //create the location header
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("{id}")
+                .buildAndExpand(newWine.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("wines/")
+    public ResponseEntity updateWine(@RequestBody Wine newWine){
+
+        //update the wine
+        boolean updated = wineDAO.updateWine(newWine);
+        if(updated)
+        {
+            //just return 200 ok
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        else {
+            //create the location header
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest().path("{id}")
+                    .buildAndExpand(newWine.getId()).toUri();
+            return ResponseEntity.created(location).build();
+        }
+    }
+}
+*/
--- src/main/java/edu/ait/winesmanager/dao/WineDAOService.java
+++ src/main/java/edu/ait/winesmanager/dao/WineDAOService.java
@@ -6,16 +6,16 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-
-@Repository
-@Transactional
-public class WineDAOService {
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public long insertWine(Wine wine)
-    {
-        entityManager.persist(wine);
-        return wine.getId();
-    }
-}
+//
+//@Repository
+//@Transactional
+//public class WineDAOService {
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//    public long insertWine(Wine wine)
+//    {
+//        entityManager.persist(wine);
+//        return wine.getId();
+//    }
+//}
